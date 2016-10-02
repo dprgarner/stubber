c9c7b828d9245ce59d3878c8b11aa1e85fd5baa8
David Garner <dprgarner@gmail.com>
2016-10-02 13:46:48 +0100

    Add tests for stub-only mode

--- test/GetComments.spec.js
+++ test/GetComments.spec.js
@@ -10,6 +10,7 @@ const rmdirSync = require('rimraf').sync;
 const GetComments = require('./GetComments');
 
 const readFile = Promise.promisify(fs.readFile);
+const writeFile = Promise.promisify(fs.writeFile);
 
 const dir = path.resolve(__dirname, 'comments');
 const APP_PORT = 3000;
@@ -17,17 +18,28 @@ const LIVE_PORT = 3001;
 const appUri = 'http://localhost:' + APP_PORT;
 const liveUri = 'http://localhost:' + LIVE_PORT;
 
+var requestsJson = [{
+  "name": "comments_postId-1",
+  "path": "comments",
+  "query": {
+    "postId": "1"
+  },
+  "file": "comments_postId-1"
+}];
+
+var responseJson = [{
+  postId: 1,
+  id: 1,
+  name: 'id labore ex et quam laborum',
+  email: 'Eliseo@gardner.biz',
+  body: 'Bello!',
+}];
+
 // Imitates the dummy live server.
 function setUpLiveServer(port, cb) {
   var app = express();
   app.get('/comments', function (req, res) {
-    res.json([{
-      postId: 1,
-      id: 1,
-      name: 'id labore ex et quam laborum',
-      email: 'Eliseo@gardner.biz',
-      body: 'Bello!',
-    }]);
+    res.json(responseJson);
   });
   return app.listen(port, cb);
 }
@@ -46,7 +58,7 @@ function setUpApp(opts, cb) {
 }
 
 function tearDownApp(cb) {
-  if (fs.existsSync(dir)) rmdirSync(dir);
+  // if (fs.existsSync(dir)) rmdirSync(dir);
   this.appServer.close(function (err) {
     if (err) return cb(err);
     this.liveServer.close(cb);
@@ -62,7 +74,7 @@ describe('GetComments in Live mode', function () {
     tearDownApp.call(this, done);
   });
 
-  it('saves a response and returns the saved response', function (done) {
+  it('saves and returns unrecognised responses', function (done) {
     request({
       uri: appUri + '/comments?postId=1',
       json: true,
@@ -73,6 +85,57 @@ describe('GetComments in Live mode', function () {
           expect(fileBody).to.deep.equal(responseJson);
           done();
         });
+    }).catch(done);
+  });
+
+  it('returns previously-saved stubs', function (done) {
+    var alternateResponse = {different: 'response'};
+    writeFile(
+      path.resolve(dir, 'comments_postId-1.json'),
+      JSON.stringify(alternateResponse)
+    ).then(function () {
+      return request({
+        uri: appUri + '/comments?postId=1',
+        json: true,
+      });
+    }).then(function (actualJson) {
+      expect(actualJson).to.deep.equal(alternateResponse);
+      done();
+    }).catch(done);
+  });
+});
+
+function createStubFiles(cb) {
+  Promise.all([writeFile(
+    path.resolve(dir, 'comments_postId-1.json'), JSON.stringify(responseJson)
+  ), writeFile(
+    path.resolve(dir, 'requests.json'), JSON.stringify(requestsJson)
+  )])
+  .then(function () {
+    cb();
+  })
+  .catch(cb);
+}
+
+describe('GetComments in stub-only mode', function () {  
+  beforeEach(function (done) {
+    setUpApp.call(this, {}, function (err) {
+      if (err) return done(err);
+      createStubFiles(done);
+    });
+  });
+
+  afterEach(function (done) {
+    tearDownApp.call(this, done);
+  });
+
+  it('returns previously-saved stubs', function (done) {
+    request({
+      uri: appUri + '/comments?postId=1',
+      json: true,
+    }).then(function (actualJson) {
+      expect(responseJson).to.deep.equal(actualJson);
+      done();
     }).catch(done);
   });
 });
