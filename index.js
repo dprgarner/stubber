554ce3b55fec1d652a742f64aedf7c9414f6a818
David Garner <dprgarner@gmail.com>
2016-10-02 19:53:40 +0100

    Add sigint catching code

--- index.js
+++ index.js
@@ -11,8 +11,30 @@ const opts = {
 var app = express();
 app.get('/favicon.ico', function (req, res) {});
 
-new GetComments(app, opts);
-new PostComments(app, opts);
+var stubbers = [
+  new GetComments(app, opts),
+  new PostComments(app, opts),
+];
 
-app.listen(PORT);
-console.log(`App listening on port ${PORT}`);
+var server = app.listen(PORT, function (err) {
+  if (err) throw err;
+  console.log(`App listening on port ${PORT}`);
+});
+
+// Not yet tested...
+process.on('SIGINT', function () {
+  console.log('Exit signal caught');
+  Promise.all(_.map(stubbers, function (stubber) {
+    return stubber.getRequestsMade()
+      .then(function(requestsMade) {
+        console.log('----');
+        console.log(`Requests made of ${stubber.name}:`);
+        console.log(JSON.stringify(requestsMade, null, 2));
+        console.log('----');
+      });
+  })).then(function () {
+    server.close(function () {
+      process.exit(0);
+    });
+  });
+});
