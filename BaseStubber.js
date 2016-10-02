26bf1b8345d830502c399883f65905b4b73dc844
David Garner <dprgarner@gmail.com>
2016-10-02 17:57:25 +0100

    Extract out more methods

--- BaseStubber.js
+++ BaseStubber.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
 const Promise = require('bluebird');
+const request = require('request-promise');
 
 const writeFile = Promise.promisify(fs.writeFile);
 const readFile = Promise.promisify(fs.readFile);
 
 function BaseStubber(app, opts) {
-  this.liveSite = opts.liveSite;
-  app.get('/:path', this.matchStub.bind(this), this.createStub.bind(this));
+  throw new Error('Not implemented');
 }
 
 Object.assign(BaseStubber.prototype, {
@@ -18,6 +19,7 @@ Object.assign(BaseStubber.prototype, {
     console.log(message);
   },
 
+  // Returns parsed requests.json.
   getRequestStubs: function() {
     return readFile(this.requestsFile)
       .catch(function (err) {
@@ -29,6 +31,31 @@ Object.assign(BaseStubber.prototype, {
       });
   },
 
+  // Finds a matching stub if any, and errors if no matching stub.
+  lookupStub: function(req) {
+    this.log(req.params.path + ', ' + JSON.stringify(req.query));
+
+    return this.getRequestStubs()
+      .then(function (stubs) {
+        for (var i = 0; i < stubs.length; i++) {
+          if (this.matches(req, stubs[i])) {
+            var filePath = path.resolve(this.directory, stubs[i].name + '.json');
+            return {name: stubs[i].name, filePath: filePath};
+          }
+        }
+      }.bind(this))
+      .then(function (stub) {
+        if (stub) {
+          this.log(`  Matched stub "${stub.name}"`);
+          return readFile(stub.filePath);
+        } else {
+          // TODO this isn't exceptional, should not be an error.
+          throw new Error('Request did not match any item stub.');
+        }
+      }.bind(this));
+  },
+
+  // Appends stub to requests.json and saves.
   saveStub: function(stub) {
     return this.getRequestStubs()
       .then(function (stubs) {
@@ -50,6 +77,27 @@ Object.assign(BaseStubber.prototype, {
         this.log(`  Error: ${err.message}`);
         return res.status(500).end(err.message);
       }.bind(this));
+  },
+
+  // Middleware which creates the request stub, saves the response stub, and
+  // returns the response.
+  saveAndReturnStub: function(req, res) {
+    var that = this;
+    this.log(`  Did not match any stub - requesting ${req.url}`);
+    var name = this.getStubName(req);
+
+    request(this.liveSite + req.url).then(function (body) {
+      return Promise.all([
+        writeFile(path.resolve(that.directory, name + '.json'), body),
+        that.saveStub(that.createStub(req, name))
+      ]).then(function () {
+        that.log(`  Saved stub '${name}'`);
+        res.type('json').end(body);
+      });
+    }).catch(function (err) {
+      that.log(`  Error: ${err.message}`);
+      res.status(500).end('Error: ' + err.message);
+    });
   },
 });
 
--- test/GetComments.js
+++ test/GetComments.js
@@ -2,18 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const _ = require('lodash');
-const Promise = require('bluebird');
-const request = require('request-promise');
 
 const BaseStubber = require('../BaseStubber');
 const queryDictsMatch = require('../utils').queryDictsMatch;
 
-const readFile = Promise.promisify(fs.readFile);
-const writeFile = Promise.promisify(fs.writeFile);
-
 function GetComments(app, opts) {
   this.liveSite = opts.liveSite;
-  app.get('/:path', this.matchStub.bind(this), this.createStub.bind(this));
+  app.get('/:path', this.matchStub.bind(this), this.saveAndReturnStub.bind(this));
   if (!fs.existsSync(this.directory)) fs.mkdirSync(this.directory);
 }
 
@@ -30,59 +25,18 @@ Object.assign(GetComments.prototype, BaseStubber.prototype, {
     return nameComponents.join('_');
   },
 
-  // TODO extract out a comparison function
-  lookupStub: function(req) {
+  matches: function(req, savedStub) {
     var itemPath = req.params.path;
     var query = req.query;
-    this.log(itemPath + ', ' + JSON.stringify(query));
-
-    return this.getRequestStubs()
-      .then(function (stubs) {
-        for (var i = 0; i < stubs.length; i++) {
-          if (
-            itemPath === stubs[i].path
-            && queryDictsMatch(query, stubs[i].query)
-          ) {
-            var file = path.resolve(this.directory, stubs[i].file + '.json');
-            return {name: stubs[i].name, file: file};
-          }
-        }
-      }.bind(this))
-      .then(function (stub) {
-        if (stub) {
-          this.log(`  Matched stub "${stub.name}"`);
-          return readFile(stub.file);
-        } else {
-          // TODO this isn't exceptional, shouldnt be an error
-          throw new Error('Request did not match any item stub.');
-        }
-      }.bind(this));
+    return itemPath === savedStub.path && queryDictsMatch(query, savedStub.query);
   },
 
-  // Middleware which creates the request stub, saves the response stub, and
-  // returns the response.
-  createStub: function(req, res) {
-    var that = this;
-    this.log(`  Did not match any stub - requesting ${req.url}`);
-    var name = this.getStubName(req);
-
-    request(this.liveSite + req.url).then(function (body) {
-      return Promise.all([
-        writeFile(path.resolve(that.directory, name + '.json'), body),
-        that.saveStub({
-          name: name,
-          path: 'comments',
-          query: req.query,
-          file: name,
-        })
-      ]).then(function () {
-        that.log(`  Saved stub '${name}'`);
-        res.type('json').end(body);
-      });
-    }).catch(function (err) {
-      that.log(`  Error: ${err.message}`);
-      res.status(500).end('Error: ' + err.message);
-    });
+  createStub: function (req, name) {
+    return {
+      name: name,
+      path: 'comments',
+      query: req.query,
+    };
   },
 });
 
