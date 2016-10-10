634f30c5f0c603516bd52b946cbaabb991a35509
David Garner <dgarner@mintel.com>
2016-10-10 22:27:54 +0100

    Add signal catching and readme

--- start.js
+++ start.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
 const path = require('path');
 
 const _ = require('lodash');
 const express = require('express');
 const program = require('commander');
 const winston = require('winston');
+
+const displayReadme = require('./utils').displayReadme;
 
 var port;
 program
@@ -22,10 +23,50 @@ if (!port) program.help();
 const opts = {};
 if (program.site) opts.liveSite = program.site;
 winston.level = (program.verbose) ? 'debug' : 'info';
-winston.add(winston.transports.File, {filename: 'log', json: false, timestamp: true});
+winston.add(winston.transports.File, {
+    filename: 'log',
+    json: false,
+    timestamp: true,
+});
+
+function closeDown() {
+  var matchedNumber = 0;
+  var unmatchedNumber = 0;
+  winston.info('Closing down Stubber server');
+  _.each(stubbers, function (stubber) {
+    var matchedRequestInfo = {
+      stubber: stubber.name,
+      matchedRequests: stubber.getMatchedRequests(),
+      unmatchedRequests: stubber.getUnmatchedRequests(),
+    };
+    winston.debug(JSON.stringify(matchedRequestInfo, null, 2));
+    matchedNumber += matchedRequestInfo.matchedRequests.length;
+    unmatchedNumber += matchedRequestInfo.unmatchedRequests.length;
+  });
+  winston.info(matchedNumber + ' matched requests');
+  winston.info(unmatchedNumber + ' unmatched requests');
+
+  server.close(function () {
+    setTimeout(function () {
+      process.exit(0);
+    }, 10)
+  });
+}
+
+process.on('SIGINT', closeDown);
+process.on('SIGTERM', closeDown);
 
 var app = express();
 app.get('/favicon.ico', function (req, res) {});
+app.get('/theme.css', function (req, res) {
+    res.sendFile(path.resolve(
+        'node_modules', 'github-markdown-css', 'github-markdown.css'
+    ));
+});
+app.get('/', function (req, res) {
+    winston.debug('Displaying readme');
+    displayReadme(res);
+});
 
 const BaseStubber = require('./BaseStubber.js');
 
@@ -42,20 +83,8 @@ var stubbers = [
 var server = app.listen(port, function (err) {
   if (err) throw err;
   winston.info('Stubber app listening on port ' + port);
+  if (program.site) {
+    winston.info('Forwarding unmatched requests to ' + program.site);
+  }
   this.setTimeout(1000);
-});
-
-process.on('SIGTERM', function () {
-  winston.info('Closing down Stubber server');
-  _.each(stubbers, function (stubber) {
-    var matchedRequestInfo = {
-      stubber: stubber.name,
-      matchedRequests: stubber.getMatchedRequests(),
-      unmatchedRequests: stubber.getUnmatchedRequests(),
-    };
-    winston.info(JSON.stringify(matchedRequestInfo, null, 2));
-  });
-  server.close(function () {
-    process.exit(0);
-  });
 });
--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
 const _ = require('lodash');
+const marked = require('marked');
+const winston = require('winston');
 
 exports.queryDictsMatch = function(dict1, dict2) {
   var keys = _.keys(dict1);
@@ -10,4 +15,31 @@ exports.queryDictsMatch = function(dict1, dict2) {
       return dict1[key] === dict2[key];
     }
   });
+};
+
+exports.displayReadme = function (res) {
+    fs.readFile(
+        path.resolve(__dirname, 'README.md'), 'utf8', function (err, data) {
+            if (err) {
+                winston.error(err.message);
+                return res.status(500).end(err.message);
+            }
+            var html = [
+                '<html><head>',
+                '<style>.markdown-body {',
+                '  box-sizing: border-box;',
+                '  min-width: 200px;',
+                '  max-width: 980px;',
+                '  margin: 0 auto;',
+                '  padding: 45px;',
+                '}</style>',
+                '<link rel="stylesheet" href="theme.css">',
+                '</head>',
+                '<body><main class="markdown-body">',
+                marked(data),
+                '</main></body>',
+            ].join('\n');
+            res.end(html);
+        }
+    );
 };
